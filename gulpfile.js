f5b7a702aed740e947db77c25877ba2062abd707
Mac <mac@Oscars-MacBook-Pro.local>
2016-05-17 14:43:15 -0500

    Agregado Compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 
@@ -12,6 +13,7 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -24,3 +26,14 @@ gulp.task('js', function(){
 	gulp.src(jsSources).pipe(concat('script.js')).pipe(browserify()).pipe(gulp.dest('builds/development/js'))
 });
 
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+	.on('error',gutil.log))
+	.pipe(gulp.dest('builds/development/css'))
+});
